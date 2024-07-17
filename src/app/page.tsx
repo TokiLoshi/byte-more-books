1ac3c938e982e28932790849a81a4d248a42b832
Bianca <103384402+TokiLoshi@users.noreply.github.com>
2024-07-17 12:00:33 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className='bg-slate-500 w-screen justify'>
+			<h1 className='text-2xl text-center'>Byte More Books</h1>
+			<p className='mt-2 text-center'>loading...</p>
+		</div>
+	);
+}

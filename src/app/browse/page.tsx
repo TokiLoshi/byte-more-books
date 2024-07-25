d9ad62a32d2414f1f5b7251fda2e7a26c1751256
Bianca <103384402+TokiLoshi@users.noreply.github.com>
2024-07-25 14:57:17 +0200

    adding book display logic

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,6 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useState, useEffect, useRef } from "react";
+
+const readingList = [
+	{
+		title: "Test Title1",
+		description: "TestDescription1",
+		pages: 234,
+		ISBN: "MadeUpISBN123",
+		readStatus: false,
+	},
+	{
+		title: "Test Title2",
+		description: "Another test description",
+		pages: 234,
+		ISBN: "MadeUpISBN124",
+		readStatus: false,
+	},
+	{
+		title: "Test Title3",
+		description: "BlahBlah Fishpaste",
+		pages: 234,
+		ISBN: "MadeUpISBN125",
+		readStatus: false,
+	},
+];
 
 export default function Browse() {
+	const [readStatus, setReadStatus] = useState(false);
+	const [formShowing, setFormShowing] = useState(false);
+	const handleMoreBooks = () => {
+		alert(`Currently form should be showing? ${formShowing}`);
+		setFormShowing(true);
+	};
+
 	return (
 		<>
 			<div className='justify-center'>
@@ -16,6 +50,17 @@ export default function Browse() {
 						<p className='text-xl text-indigo-500 text-center'>Reading List</p>
 					</Link>
 				</div>
+			</div>
+			<div className='shadow m-4 p-2 rounded justify-center text-center bg-indigo-500'>
+				{readingList.map((book) => (
+					<div key={book.ISBN} className='mb-4 shadow bg-zinc-800 p-5 rounded'>
+						<h1 className='text-xl'>{book.title}</h1>
+						<p>{book.description}</p>
+						<p>{book.readStatus}</p>
+					</div>
+				))}
+				<Button onClick={handleMoreBooks}>Add More!</Button>
+				{formShowing && <p>Stuff to conditionally render</p>}
 			</div>
 		</>
 	);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 		<>
 			<div className=' bg-slate-500 w-screen justify'>
 				<h1 className='text-2xl text-center'>Byte More Books</h1>
-				<p className='mt-2 text-center'>loading...</p>
+				<p className='mt-2 text-center'>Home Page...</p>
 			</div>
 			<div className='justify-center m-2'>
 				<Link href='/profile'>
@@ -15,7 +15,25 @@ export default function Home() {
 						Profile Page
 					</p>
 				</Link>
-				<Button variant='outline'>Shadcn Button</Button>
+				<div className='justify-center m-2'>
+					<Link href='/browse'>
+						<p className='text-2xl text-indigo-500 text-center m-2 '>
+							Browse Books
+						</p>
+					</Link>
+				</div>
+				<div className='justify-center m-2'>
+					<Link href='/reading-list'>
+						<p className='text-2xl text-indigo-500 text-center m-2 '>
+							Reading-List
+						</p>
+					</Link>
+				</div>
+				<div className='text-center'>
+					<Button variant='outline' className=' hover:bg-indigo-500'>
+						Shadcn Button
+					</Button>
+				</div>
 			</div>
 		</>
 	);
